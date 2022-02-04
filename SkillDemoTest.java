3ad3c5cff9525a4854cebc62c2971d6cf3638dad
Monip1 <elena@ratmotor.com>
2022-02-04 15:09:07 -0800

    fixed test so it will pass

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void concatinating(){
-        assertEquals("good dog", SkillDemo.concat("good", "dog"));
+        assertEquals("good dog", SkillDemo.concat("good ", "dog"));
     }
 }
 
